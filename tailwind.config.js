981aa5731a9991517c1f2702067f45e8beec15ee
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-09-10 22:57:03 +0100

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'home': 'DM Sans, sans-serif',
+      'detail': 'Poppins, sans-serif',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
